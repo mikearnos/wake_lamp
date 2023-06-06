2676f7e1e4ffac9e8dc6b2b129cfccbc60ac8451
Mike Arnos <mikearnos@gmail.com>
2023-06-06 04:12:40 -0400

    added the PCF8574 interrupt

--- include/outlet_control.h
+++ include/outlet_control.h
@@ -1,6 +1,9 @@
 #define OUTLET_NUM    4
 #define OUTLET_START P0 // using PCF8574 output pins P0 - P3
+#define INPUT_START P4 // using PCF8574 input pins P4 - P7
 #define PCF8574_ADDR  0x20
+
+#define PCF8574_INT_PIN D5 // wemos D1 mini pin
 
 void outletHardwareSetup(void);
 void outletGetStatus(uint8_t*);
