2932a94ea015acd6d7a321f080f61c03a55fcccd
Mike Arnos <mikearnos@gmail.com>
2023-06-09 04:03:33 -0400

    added scroll dot and frame around the time

--- src/oled.cpp
+++ src/oled.cpp
@@ -1,15 +1,26 @@
 #include <U8g2lib.h>
 U8G2_SH1106_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0);
+/*Note: In v2.29 the returned value might be (a little bit) larger than in
+  the previous versions of u8g2. Undef U8G2_BALANCED_STR_WIDTH_CALCULATION
+  in u8g2.h to restore the old behaviour.*/
 
 void oledSetup(void)
 {
     u8g2.begin();
+    u8g2.setFont(u8g2_font_ncenB24_tr);
 }
 
-void oledGo(char* string)
+void oledGo(char* string, int value, int horizontal)
 {
     u8g2.clearBuffer();
-    u8g2.setFont(u8g2_font_ncenB14_tr);
-    u8g2.drawStr(0, 20, string);
+    //u8g2.drawFrame(3,7,25,15);
+    if (value == 0) {
+        u8g2.drawButtonUTF8(62, 30, U8G2_BTN_HCENTER | U8G2_BTN_BW2, 0, 3, 3, string);
+    }
+    if (value == 1) {
+        u8g2.drawButtonUTF8(62, 30, U8G2_BTN_SHADOW1 | U8G2_BTN_INV | U8G2_BTN_HCENTER | U8G2_BTN_BW2, 34, 2, 2, string);
+    }
+    u8g2.drawLine(horizontal, 61, horizontal, 63);
+    //u8g2.drawStr(0, 20, string);
     u8g2.sendBuffer();
 }
