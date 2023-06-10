f45aad5cc677f06bc29dc257cb6d34b18dc8ed05
Mike Arnos <mikearnos@gmail.com>
2023-06-10 04:59:59 -0400

    use AM/PM as a slide indicator

--- src/oled.cpp
+++ src/oled.cpp
@@ -12,15 +12,32 @@ void oledSetup(void)
 
 void oledGo(char* string, int value, int horizontal)
 {
+    int strWidth;
     u8g2.clearBuffer();
     //u8g2.drawFrame(3,7,25,15);
+    u8g2.setFont(u8g2_font_ncenB24_tr);
     if (value == 0) {
         u8g2.drawButtonUTF8(62, 30, U8G2_BTN_HCENTER | U8G2_BTN_BW2, 0, 3, 3, string);
     }
     if (value == 1) {
         u8g2.drawButtonUTF8(62, 30, U8G2_BTN_SHADOW1 | U8G2_BTN_INV | U8G2_BTN_HCENTER | U8G2_BTN_BW2, 34, 2, 2, string);
     }
-    u8g2.drawLine(horizontal, 61, horizontal, 63);
+
+    u8g2.setFont(u8g2_font_ncenB10_tr);
+
+    if (horizontal < 256) {
+        horizontal = constrain(horizontal, 0, 255);
+        horizontal = map(horizontal, 0, 255, 0, 127 - u8g2.getStrWidth("AM"));
+        u8g2.drawStr(horizontal, 63, "AM"); // 24 pixels wide
+    } else {
+        horizontal -= 256;
+        horizontal = constrain(horizontal, 0, 255);
+        horizontal = map(horizontal, 0, 255, 0, 127 - u8g2.getStrWidth("PM"));
+        u8g2.drawStr(horizontal, 63, "PM"); // 23 pixels wide
+    }
+
+    //u8g2.drawLine(horizontal, 61, horizontal, 63);
     //u8g2.drawStr(0, 20, string);
     u8g2.sendBuffer();
+    //Serial.printf("%d %d\n", u8g2.getStrWidth("AM"), u8g2.getStrWidth("PM"));
 }
