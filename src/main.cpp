12aad0a9d12d1fe1f072c5c9de23891dd38da44c
Mike Arnos <mikearnos@gmail.com>
2023-08-12 13:34:55 -0400

    moved analog functions to own file

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,16 @@
 #include "clock_ds3231.h"
 #include "time_ntp.h"
 #include "outlet_control.h"
+#include "analog.h"
 
 #include "..\..\..\wifi.h"
 const char* ssid = STASSID;
 const char* password = STAPSK;
 
-const int analogInPin = A0; // ESP8266 Analog Pin ADC0 = A0
-int sensorValue = 0; // value read from the pot
-
 extern bool clockUpdateTime;
 
 extern void oledSetup();
 extern void oledGo(char*, int, int);
-int bufferADC(int);
-int movingWindowADC(int);
 
 void setup()
 {
@@ -42,71 +38,30 @@ void setup()
 
 void loop()
 {
-    char bufferStr[7]; //the ASCII of the integer will be stored in this char array
+    int sensorValue; // value read from the pot
+
+    char bufferStr[6]; //the ASCII of the time will be stored in this char array
     const char* hourStrings[] = { "12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11" };
     const char* minStrings[] = { "00", "30" };
-    //const char* noonStrings[] = { "AM", "PM" };
     //sensorValue = bufferADC(8);
     sensorValue = movingWindowADC(4);
-    sensorValue = constrain(sensorValue, 20, 1024);
-    int horizontalValue = map(sensorValue, 20, 1024, 0, 511);
-    sensorValue = map(sensorValue, 20, 1024, 0, 47);
+    sensorValue = constrain(sensorValue, DEADZONE_LOW, DEADZONE_HIGH); // trim the dead zone
+    int horizontalValue = map(sensorValue, DEADZONE_LOW, DEADZONE_HIGH, 0, 511);
+    sensorValue = map(sensorValue, DEADZONE_LOW, DEADZONE_HIGH, 0, 47); // amount of 30 minute slices in the day
 
     //itoa(sensorValue, bufferStr, 10); //(integer, yourBuffer, base)
-
-    static uint32_t oledRefresh = millis();
 
     strcpy(bufferStr, hourStrings[sensorValue / 2]);
     strcat(bufferStr, ":");
     strcat(bufferStr, minStrings[sensorValue % 2]);
-    //strcat(bufferStr, " ");
-    //strcat(bufferStr, noonStrings[(sensorValue / 24) % 2]);
 
-    //delay(20);
+    static uint32_t oledRefresh = millis();
     if (millis() - oledRefresh > 30) {
         oledRefresh = millis();
         oledGo(bufferStr, (sensorValue / 24) % 2, horizontalValue);
     }
 
     clockCheckEvents(); // launch events once a minute and once a month
-}
-
-int bufferADC(int bufferSize)
-{
-#define MAX_BUFFER_SIZE 16
-    static int buffer[MAX_BUFFER_SIZE];
-    int average = 0;
-
-    if (bufferSize > MAX_BUFFER_SIZE) {
-        return 0;
-    }
-    for (int i = 0; i < bufferSize - 1; i++) { // shift values before inserting new value
-        buffer[i] = buffer[i + 1];
-    }
-    buffer[bufferSize - 1] = analogRead(analogInPin); // insert new value at the end
-
-    for (int i = 0; i < bufferSize; i++) { // average the buffer
-        average += buffer[i];
-    }
-    average = average / bufferSize;
-
-    //Serial.printf("raw: %d buffer: %d ", buffer[bufferSize - 1], average);
-
-    return average;
-}
-
-int movingWindowADC(int deviation)
-{
-    static int stableRead;
-
-    int newRead = analogRead(analogInPin);
-    if (newRead >= stableRead + deviation || newRead <= stableRead - deviation) {
-        stableRead = newRead;
-    }
-
-    //Serial.printf("movingWindow: %d\n", stableRead);
-
-    return stableRead;
 }
 
 void handleEventMinutes(void)
