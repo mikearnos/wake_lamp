663aeb6e9df52fa043b1e247019d479e955ba2bb
Mike Arnos <mikearnos@gmail.com>
2023-08-30 10:55:02 -0400

    auto formatted

--- include/clock_ds3231.h
+++ include/clock_ds3231.h
@@ -4,19 +4,19 @@
 #define DS3231_INT_PIN D6 // wemos D1 mini pin
 
 // self defined error values
-#define DS3231_OK         0b00000000
-#define DS3231_NO_DETECT  0b00000001
+#define DS3231_OK 0b00000000
+#define DS3231_NO_DETECT 0b00000001
 #define DS3231_LOST_POWER 0b00000010
 
 // Set timer delay values
-#define ALARM1_DATE   1        // first day of the month
-#define ALARM1_HOUR   0        // midnight
+#define ALARM1_DATE 1 // first day of the month
+#define ALARM1_HOUR 0 // midnight
 #define ALARM1_MINUTE 0
-#define ALARM1_BITS 0b0000     // "Alarm when date, hours, minutes, and seconds match"
+#define ALARM1_BITS 0b0000 // "Alarm when date, hours, minutes, and seconds match"
 #define ALARM2_BITS 0b111 << 4 // "Alarm once per minute (00 seconds of every minute)"
 
 #define NTP_DELAY_MINS 120
-#define NTP_DELAY_HOURS ( NTP_DELAY_MINS / 60 )
+#define NTP_DELAY_HOURS (NTP_DELAY_MINS / 60)
 
 extern bool clockUpdateTime;
 
@@ -28,5 +28,8 @@ void clockSetAlarms(void);
 void clockHandleEventMinutes(void);
 void clockNTPUpdate(int16_t);
 void clockSetEpoch(time_t);
-char * clockGetTimeDateString(time_t);
+char* clockGetTimeDateString(time_t);
+char* clockGetTimeString(time_t);
 time_t clockGetLocalTime(void);
+
+extern char* clockTimeString;
